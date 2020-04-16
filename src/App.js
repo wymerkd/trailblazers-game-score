cc487b2218a20a18b73d6af45f613c618b26317f
wymerkd <wymerkd@gmail.com>
2020-04-15 23:29:23 -0700

    add team 1, versus, and team 2 columns, adjust padding

--- src/App.js
+++ src/App.js
@@ -8,12 +8,25 @@ function App() {
       <div className="adContainer">
         <h3>Ad</h3>
       </div>
-      <div className="col-xs-12 scoreContainer">
-        <div className="col-xs-12 gameHeader">
-          <h1>Game Title Info Arena</h1>
+      <div className="container scoreContainer">
+        <div className="row">
+          <div className="col-xs-12 gameHeader">
+            <h1>Game Title Info Arena</h1>
+          </div>
         </div>
-        
-         
+        <div className="container gameContainer">
+          <div className="row">
+            <div className="firstTeam col-5">
+              <h1>Team 1</h1>
+            </div>
+            <div className="versus col-2">
+              <h1>VS</h1>
+            </div>
+            <div className="secondTeam col-5">
+              <h1>Team 2</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
