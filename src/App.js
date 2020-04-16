c4eb3c9fb305e37e96e669f0f45850b9ddc2d367
wymerkd <wymerkd@gmail.com>
2020-04-16 08:33:07 -0700

    add font-family to header, add team logo placeholders, and adjust padding accordingly

--- src/App.js
+++ src/App.js
@@ -11,18 +11,20 @@ function App() {
       <div className="container scoreContainer">
         <div className="row">
           <div className="col-xs-12 gameHeader">
-            <h1>Game Title Info Arena</h1>
+            <h1>Game Title | Info |  Arena</h1>
           </div>
         </div>
         <div className="container gameContainer">
           <div className="row">
             <div className="firstTeam col-5">
+              <img src="" alt="team 1 logo"></img>
               <h1>Team 1</h1>
             </div>
             <div className="versus col-2">
               <h1>VS</h1>
             </div>
             <div className="secondTeam col-5">
+              <img src="" alt="team 2 logo"></img>
               <h1>Team 2</h1>
             </div>
           </div>
