6501603e0193e0f876263fefda19437bd6cb54e1
wymerkd <wymerkd@gmail.com>
2020-04-15 17:37:11 -0700

    copy over app.js and app.css content from previous project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="mainContainer">
+        <div className="adContainer">
+          <h3>Ad</h3>
+        </div>
+        <div className="scoreContainer">
+          <div>
+            <h1>Game Title  - Info - Arena</h1>
+          </div>
+          <div className="teamContainer">
+            <div className="displayedTeam">
+              <img src="https://via.placeholder.com/500x350" alt="team logo"/>
+            </div>
+            <div className="versusCircle">
+              <h3>VS</h3>
+            </div>
+            <div className="displayedTeam">
+              {/* <img src="https://via.placeholder.com/500x350" alt="team logo"/> */}
+              <p>hey</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>    
   );
 }
-
 export default App;
