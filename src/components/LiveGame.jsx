df53a79088a312823b664be06de217cac98ab40f
wymerkd <wymerkd@gmail.com>
2020-04-17 08:21:20 -0700

    adjust padding and add background and border colors to live game scoreboard

--- src/components/LiveGame.jsx
+++ src/components/LiveGame.jsx
@@ -13,24 +13,23 @@ export default function LiveGame(props) {
           <div className="col-xs-12 gameHeader">
             <h2>Game Title | Info |  Arena</h2>
           </div>
-          {/* <img className="headerDivider" src="https://www.nba.com/blazers/sites/blazers/files/pageheaderbar_50thanniversary.jpg" alt="page header divider"></img> */}
         </div>
-        <div className="container gameContainer">
-          <div className="row">
+        <div className="container teamsContainer">
+          <div className="scoreBox">
+            <p>Scorebox Card</p>
+            <h4>101 - 98</h4>
+          </div>
+          <div className="row rowPadding">
             <div className="firstTeam col-5">
               <img src={props.gameData.firstTeamLogo} alt="first team logo"></img>
-              <h1>{props.gameData.firstTeam}</h1>
+              <h2>{props.gameData.firstTeam}</h2>
             </div>
             <div className="versus col-2">
               <h1>VS</h1>
             </div>
             <div className="secondTeam col-5">
               <img src={props.gameData.secondTeamLogo} alt="second team logo"></img>
-              <h1>{props.gameData.secondTeam}</h1>
-              <div className="scoreBox">
-                <p>Scorebox Card</p>
-                <h4>101-98</h4>
-              </div>
+              <h2>{props.gameData.secondTeam}</h2>
             </div>
           </div>
         </div>
