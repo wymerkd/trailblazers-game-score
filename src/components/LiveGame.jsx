a5c12ff5e708161594fb8deba2df984dbbd92a92
wymerkd <wymerkd@gmail.com>
2020-04-24 11:57:35 -0700

    wrap all content in a single div that will be hidden in mobile view, add comments to clarify

--- src/components/LiveGame.jsx
+++ src/components/LiveGame.jsx
@@ -7,67 +7,71 @@ export default function LiveGame(props) {
   return (
     <div className="liveGameMainContainer">
       <div className="container liveGameScoreContainer">
-        <div className="row liveGameHeaderRow">
-          <div className="col-xs-12 liveGameHeader">
-            <h2>Game Title | Info |  Arena</h2>
+        {/* Content for desktop view only */}
+        <div className="liveGameDesktopOnly">
+          <div className="row liveGameHeaderRow">
+            <div className="col-xs-12 liveGameHeader">
+              <h2>Game Title | Info |  Arena</h2>
+            </div>
+          </div>
+          <div className="container liveGameTeamsContainer">
+            <div className="liveGameScoreBox">
+              <p>Scorebox Card</p>
+              <h4>101 - 98</h4>
+            </div>
+            <div className="row rowPadding">
+              <div className="liveGameFirstTeam col-5">
+                <img src={props.gameData.firstTeamLogo} alt="first team logo"></img>
+                <h2>{props.gameData.firstTeam}</h2>
+              </div>
+              <div className="versus col-2">
+                <h1>VS</h1>
+              </div>
+              <div className="liveGameSecondTeam col-5">
+                <img src={props.gameData.secondTeamLogo} alt="second team logo"></img>
+                <h2>{props.gameData.secondTeam}</h2>
+              </div>
+            </div>
+          </div>
+          <div className="container liveGameScoreFeedContainer">
+            <div className="row">
+              <div className="col-12">
+                <p>Score Feed</p>
+              </div>
+              <div className="row scoreFeedBoxes">
+                <div className="col">
+                  <p>Game</p>
+                  <p>101 - 98</p>
+                </div>
+                <div className="col">
+                  <p>Game</p>
+                  <p>101 - 98</p>
+                </div>
+                <div className="col">
+                  <p>Game</p>
+                  <p>101 - 98</p>
+                </div>
+                <div className="col">
+                  <p>Game</p>
+                  <p>101 - 98</p>
+                </div>
+                <div className="col">
+                  <p>Game</p>
+                  <p>101 - 98</p>
+                </div>
+                <div className="col">
+                  <p>Game</p>
+                  <p>101 - 98</p>
+                </div>
+                <div className="col noBorderRight">
+                  <p>Game</p>
+                  <p>101 - 98</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="container liveGameTeamsContainer">
-          <div className="liveGameScoreBox">
-            <p>Scorebox Card</p>
-            <h4>101 - 98</h4>
-          </div>
-          <div className="row rowPadding">
-            <div className="liveGameFirstTeam col-5">
-              <img src={props.gameData.firstTeamLogo} alt="first team logo"></img>
-              <h2>{props.gameData.firstTeam}</h2>
-            </div>
-            <div className="versus col-2">
-              <h1>VS</h1>
-            </div>
-            <div className="liveGameSecondTeam col-5">
-              <img src={props.gameData.secondTeamLogo} alt="second team logo"></img>
-              <h2>{props.gameData.secondTeam}</h2>
-            </div>
-          </div>
-        </div>
-        <div className="container liveGameScoreFeedContainer">
-          <div className="row">
-            <div className="col-12">
-              <p>Score Feed</p>
-            </div>
-            <div className="row scoreFeedBoxes">
-              <div className="col">
-                <p>Game</p>
-                <p>101 - 98</p>
-              </div>
-              <div className="col">
-                <p>Game</p>
-                <p>101 - 98</p>
-              </div>
-              <div className="col">
-                <p>Game</p>
-                <p>101 - 98</p>
-              </div>
-              <div className="col">
-                <p>Game</p>
-                <p>101 - 98</p>
-              </div>
-              <div className="col">
-                <p>Game</p>
-                <p>101 - 98</p>
-              </div>
-              <div className="col">
-                <p>Game</p>
-                <p>101 - 98</p>
-              </div>
-              <div className="col noBorderRight">
-                <p>Game</p>
-                <p>101 - 98</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        {/* end */}
         {/* Containers for mobile only */}
         <div className="container liveGameMobileVideo">
           <div className="row">
